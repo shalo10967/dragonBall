dce9d818d758b844132714d64a4ced0debbfe6ea
Juan Martinez <juangabrielm@consentio.co>
2025-07-15 17:02:07 -0500

    add convenios analysis

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
-import { Crud, DragonBall, Home } from './screens';
+import { Crud, DragonBall, Home, ConveniosAnalysis } from './screens';
 
 const App: React.FC = () => {
   return (
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       <CssBaseline />
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ConveniosAnalysis />} />
           <Route path="/dragonBall" element={<DragonBall />} />
           <Route path="/crud" element={<Crud />} />
         </Routes>
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,9 +1,10 @@
 import { DragonBall } from './DragonBall/DragonBall';
 import { Crud } from './Crud/Crud';
 import { Home } from './Home/Home';
-
+import { ConveniosAnalysis } from './ConveniosAnalysis/ConveniosAnalysis';
 export {
     DragonBall,
     Crud,
-    Home
+    Home,
+    ConveniosAnalysis
 }
